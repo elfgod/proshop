806dd07a02784dcd1122d08d00d0beda1957d4cb
elfgod <albertogx@icloud.com>
2021-11-10 00:17:11 -0500

    User Authentication Endpoint

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
   name: {
@@ -18,10 +19,16 @@ const userSchema = mongoose.Schema({
     type: Boolean,
     required: true,
     default: false
+    }
+  },
+  {
+    timestamps: true
   }
-}, {
-  timestamps: true
-})
+)
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 
 const User = mongoose.model('User', userSchema)
 

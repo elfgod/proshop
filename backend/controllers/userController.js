7e2f7937c4f789b75b81a7156b94afd4bfc4adef
elfgod <albertogx@icloud.com>
2021-11-23 19:34:25 -0500

    Update Profile Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,9 +2,9 @@ import asyncHandler from 'express-async-handler'
 import User from '../models/userModel.js'
 import generateToken from '../utils/generateToken.js'
 
-// @description Auth user & get token
-// @route POST /api/users/login
-// @access Public
+// @description   Auth user & get token
+// @route         POST /api/users/login
+// @access        Public
 const authUser = asyncHandler(async (req, res) => {
   
   const { email, password } = await req.body
@@ -25,9 +25,9 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @description Register a new user
-// @route POST /api/users
-// @access Public
+// @description   Register a new user
+// @route         POST /api/users
+// @access        Public
 const registerUser = asyncHandler(async (req, res) => {
   
   const { name, email, password } = await req.body
@@ -61,9 +61,9 @@ const registerUser = asyncHandler(async (req, res) => {
 
 })
 
-// @description Get user profile
-// @route GET /api/users/profile
-// @access Private
+// @description   Get user profile
+// @route         GET /api/users/profile
+// @access        Private
 const getUserProfile = asyncHandler(async (req, res) => {
   const user = await User.findById(req.user._id)
   
@@ -80,4 +80,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 })
 
-export { authUser, registerUser, getUserProfile }
+// @description   Update user profile
+// @route         PUT /api/users/profile
+// @access        Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  
+  if (user) {
+   user.name = req.body.name || user.name
+   user.email = req.body.email || user.email
+   if(req.body.password) {
+     user.password = req.body.password
+   }
+
+   const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id)
+    })
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
